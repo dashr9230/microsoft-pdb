b37fda8929292b2af536662a2ee2dc30974b4a7c
RD42 <42702181+dashr9230@users.noreply.github.com>
2024-01-18 23:57:25 +0800

    Adding stubs to type7.cpp

--- cvdump/type7.cpp
+++ cvdump/type7.cpp
@@ -1,1 +1,39 @@
 
+#include "cvdump.h"
+
+BYTE RecBuf[MAXTYPE];
+
+
+void DumpCom()
+{
+	// TODO: DumpCom
+
+	StdOutPrintf(L"DumpCom: Not Implemented.");
+}
+
+
+void DumpModTypC7(size_t cbTyp)
+{
+	// TODO: DumpModTypC7
+
+	StdOutPrintf(L"DumpModTypC7: Not Implemented.");
+}
+
+
+void DumpTyp()
+{
+	// TODO: DumpTyp
+
+	StdOutPrintf(L"DumpTyp: Not Implemented.");
+}
+
+
+CV_typ_t DumpTypRecC7(CV_typ_t a, WORD b, BYTE* c, TPI* d, PDB* e)
+{
+	// TODO: DumpTypRecC7
+
+	StdOutPuts(L"DumpTypRecC7: Not implemented.");
+
+	return 0;
+}
+
