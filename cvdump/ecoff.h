2f0ef74562a4752f6496a889250eaaf58bc97ed2
RD42 <42702181+dashr9230@users.noreply.github.com>
2024-03-04 23:50:59 +0800

    Update ecoff.h

--- cvdump/ecoff.h
+++ cvdump/ecoff.h
@@ -3,5 +3,8 @@
 // ECOFF.H
 //------------------------------------------------------------------------------
 
+#define INITGUID
+#include <guiddef.h>
+
 // {D1BAA1C7-BAEE-4ba9-AF20-FAF66AA4DCB8}
 DEFINE_GUID(EXTENDED_COFF_OBJ_GUID, 0xd1baa1c7, 0xbaee, 0x4ba9, 0xaf, 0x20, 0xfa, 0xf6, 0x6a, 0xa4, 0xdc, 0xb8);
